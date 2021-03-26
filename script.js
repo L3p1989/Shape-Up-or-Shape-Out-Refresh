679b741f624d220ab0b3bead20b71f050461bcb7
L3p1989 <phogan110@live.com>
2021-03-26 13:44:39 -0700

    created variables for all span elements

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//assign new variable shapeTxt as #shape-txt element
+const shapeTxt = document.getElementById("shape-txt");
+//assign new variable shapeWidth as #width-txt element
+const shapeWidth = document.getElementById("width-txt");
+//assign new variable shapeHeight as #height-txt element
+const shapeHeight = document.getElementById("height-txt");
+//assign new variable shapeRadius as #radius-txt element
+const shapeRadius = document.getElementById("radius-txt");
+//assign new variable shapeArea as #area-txt element
+const shapeArea = document.getElementById("area-txt");
+//assign new variable shapePerimeter as #perimeter-txt element
+const shapePerimeter = document.getElementById("perimeter-txt");
+
 //assign Shape as new class
 class Shape {
   constructor(height, width) {
