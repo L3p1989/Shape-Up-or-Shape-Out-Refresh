8cfed71fa1f67c1e9ebce382fe128afd5f40eabd
L3p1989 <phogan110@live.com>
2021-03-26 16:19:28 -0700

    assigned canvas variable as #shape-canvas. assigned calcX/calcY methods to help with shape positioning. assigned Circle as new class child of Shape

--- script.js
+++ script.js
@@ -1,20 +1,69 @@
-//assign new variable shapeTxt as #shape-txt element
 const shapeTxt = document.getElementById("shape-txt");
-//assign new variable shapeWidth as #width-txt element
+
 const shapeWidth = document.getElementById("width-txt");
-//assign new variable shapeHeight as #height-txt element
+
 const shapeHeight = document.getElementById("height-txt");
-//assign new variable shapeRadius as #radius-txt element
+
 const shapeRadius = document.getElementById("radius-txt");
-//assign new variable shapeArea as #area-txt element
+
 const shapeArea = document.getElementById("area-txt");
-//assign new variable shapePerimeter as #perimeter-txt element
+
 const shapePerimeter = document.getElementById("perimeter-txt");
 
-//assign Shape as new class
+const canvas = document.getElementById("shape-canvas");
+
 class Shape {
   constructor(height, width) {
     this.height = height;
     this.width = width;
+  }
+
+  calcX = () => {
+    let min = 362;
+    let max = 942;
+    //sum is range between 362 and 942
+    let sum = Math.floor(Math.random() * (max - min + 1) + min);
+    return sum;
+  };
+
+  calcY = () => {
+    let min = 223;
+    let max = 797;
+    //sum is range between 223 and 797
+    let sum = Math.floor(Math.random() * (max - min + 1) + min);
+    return sum;
+  };
+}
+
+class Circle extends Shape {
+  constructor(radius) {
+    super(radius, radius);
+
+    this.newCircle = document.createElement("div");
+    this.newCircle.classList.add("circle");
+    this.newCircle.style.width = `${radius * 2}px`;
+    this.newCircle.style.height = `${radius * 2}px`;
+    this.newCircle.style.left = `${this.calcX()}px`;
+    this.newCircle.style.top = `${this.calcY()}px`;
+
+    this.newCircle.addEventListener("click", () => {
+      const diameter = radius * 2;
+      const circleTxt = document.createTextNode(`${this.constructor.name}`);
+      shapeTxt.appendChild(circleTxt);
+      const radiusTxt = document.createTextNode(radius);
+      shapeRadius.appendChild(radiusTxt);
+      const circlePerimeter = Math.round(diameter * Math.PI);
+      const perimeterTxt = document.createTextNode(circlePerimeter);
+      shapePerimeter.appendChild(perimeterTxt);
+      const circleArea = Math.round(radius * radius * Math.PI);
+      const areaTxt = document.createTextNode(circleArea);
+      shapeArea.appendChild(areaTxt);
+    });
+
+    this.newCircle.addEventListener("dblclick", () => {
+      this.newCircle.remove();
+    });
+
+    canvas.appendChild(this.newCircle);
   }
 }
